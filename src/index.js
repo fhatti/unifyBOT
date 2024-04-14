db366888295880b15a8e495e32785fe66f4b99ae
Purcariu Vlad <purcariuvlad@Purcarius-Mac-mini.local>
2024-04-14 15:04:19 +0200

    Moved .Ds_Store to .gitignore + create Profile Embed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
-const { Client, IntentsBitField, Embed, EmbedBuilder, MessageEmbed , CommandInteraction } = require("discord.js");
+const {
+  Client,
+  IntentsBitField,
+  Embed,
+  EmbedBuilder,
+  MessageEmbed,
+  CommandInteraction,
+} = require("discord.js");
 
 const client = new Client({
   intents: [
@@ -56,9 +63,31 @@ client.on("interactionCreate", async (interaction) => {
   }
 
   if (interaction.commandName === "profile") {
-    console.log("salut");
+    const userUsername = interaction.user.username;
+    const avatarUrl = interaction.user.displayAvatarURL();
+    console.log(avatarUrl);
+    const profileEmbed = new EmbedBuilder()
+      .setAuthor({
+        name: userUsername,
+        iconURL:
+          "https://scontent-muc2-1.xx.fbcdn.net/v/t39.30808-6/369726576_1958035307911719_7377222010976924482_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=COVPR9aEqtMAb4YylAp&_nc_ht=scontent-muc2-1.xx&oh=00_AfDNSX4pXiya8oFxEW9AYSQn2bVYUEQZu5Zb4218DHNRvA&oe=6621B191",
+      })
+      .setTitle("My First title")
+      .setDescription("Description")
+      .setThumbnail(avatarUrl)
+      .addFields(
+        { name: "1st Field title", value: "Value" },
+        { name: "2nd Field title", value: " value" },
+        { name: "\u200B", value: "\u200B" },
+        { name: "1st Inline Field", value: "value", inline: true },
+        { name: "2nd Inline Field", value: "value", inline: true },
+        { name: "3rd Inline Field", value: "value", inline: true }
+      )
+      .setImage("https://i.imgur.com/AfFp7pu.png")
+      .setFooter({ text: "Footer TExt " })
+      .setTimestamp();
+    interaction.reply({ embeds: [profileEmbed] });
   }
-
 });
 
 client.login(process.env.TOKEN);
